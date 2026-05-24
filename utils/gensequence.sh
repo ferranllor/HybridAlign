75323bfe9c63e566068d7c4a46f4ba6a0ef6b443
Ferran Llorà <ferranllor@gmail.com>
2026-05-24 02:53:56 +0200

    Added some bigger sequences

--- utils/gensequence.sh
+++ utils/gensequence.sh
@@ -1,8 +1,8 @@
 # 1. Simulate reads and output their alignments (.gam)
-./vg sim -n 1000 -l 150 -x ../HybridAlign/datasets/graphs/"$1".gfa -a > "$1".gam
+./vg sim -n 1000 -l 4500 -x ../datasets/graphs/"$1".gfa -a > "$2".gam
 
 # 2. Extract standard FASTQ sequences
-./vg view -X "$1".gam > "$1".fq
+./vg view -X "$2".gam > "$2".fq
 
 # 3. Extract Read ID and the Ground Truth Sequence into a TSV file
-./vg view -aj "$1".gam | jq -r '[.name, .sequence] | @tsv' > "$1".tsv
+./vg view -aj "$2".gam | jq -r '[.name, .sequence] | @tsv' > "$2".tsv
